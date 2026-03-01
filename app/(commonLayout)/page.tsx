389bdf8a8638c902b194295b2224628d84d8b699
aiarnob23 <aiarnob23@gmail.com>
2026-03-01 15:33:59 +0600

    trusted by section updated

--- app/(commonLayout)/page.tsx
+++ app/(commonLayout)/page.tsx
@@ -1,10 +1,13 @@
 import Banner from "@/components/commonLayout/Home/Banner";
+import { TrustedBy } from "@/components/commonLayout/Home/TrustedBy";
+
 
 
 export default function HomePage() {
     return (
         <div className="">
             <Banner/>
+            <TrustedBy title="Companies we helped grow"/>
         </div>
     );
 }
