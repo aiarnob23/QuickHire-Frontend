bfaa391160c3dd7d62d43ad30e2530b5f41b2d34
aiarnob23 <aiarnob23@gmail.com>
2026-03-02 11:17:18 +0600

    metadata , font updated

--- lib/baseUrl.ts
+++ lib/baseUrl.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const serverBaseUrl = axios.create({
-    baseURL:"http://localhost:4000/api",
+    //baseURL:"http://localhost:4000/api",
+    baseURL:"https://quickhire-backend-production-e3a4.up.railway.app/api",
     withCredentials:true,
 }) 
--- services/authService.ts
+++ services/authService.ts
@@ -6,11 +6,5 @@ import { serverBaseUrl } from "@/lib/baseUrl";
  */
 export const loginAdmin = async (loginData: any) => {
     const response = await serverBaseUrl.post('/auth/login', loginData);
-    
-    // Store token if login is successful
-    if (response.data?.data?.accessToken) {
-        localStorage.setItem("accessToken", response.data.data.accessToken);
-    }
-    
     return response.data;
 };
